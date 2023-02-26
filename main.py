2447912127318a32813d04487f9d77d603171b79
Admintester838 <125737238+Admintester838@users.noreply.github.com>
2023-02-26 18:11:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class Calculations:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_sum(self):
+        return self.a + self.b
+
+    def get_difference(self):
+        return self.a - self.b
+
+    def get_product(self):
+        return self.a * self.b
+
+    def get_quotient(self):
+        return self.a / self.b
+
